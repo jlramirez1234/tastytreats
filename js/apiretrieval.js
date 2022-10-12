fddaded8b1018fae473c27515916bf503f803f8d
Jonathan Ramirez <77942993+jlramirez1234@users.noreply.github.com>
2022-10-11 20:33:27 -0500

    initial commit

---
+++ js/apiretrieval.js
@@ -0,0 +1,5 @@
+const desserts = fetch(`https://freerandomapi.cyclic.app/api/v1/desserts`);
+const cookies = fetch('https://freerandomapi.cyclic.app/api/v1/desserts?category=Cookie');
+const donuts = fetch ('https://freerandomapi.cyclic.app/api/v1/desserts?category=Donut');
+const iceCream = fetch ('https://freerandomapi.cyclic.app/api/v1/desserts?category=Ice_Cream');
+
